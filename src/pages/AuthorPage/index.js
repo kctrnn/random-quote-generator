897cc15444ff50c14b503149f33300d34117a208
kctrnn <kctrnn@gmail.com>
2021-05-05 22:17:49 +0700

    author page

--- src/pages/AuthorPage/index.js
+++ src/pages/AuthorPage/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useParams } from 'react-router';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router';
+import quoteApi from '../../api/quoteApi';
+import QuoteList from '../../components/QuoteList';
+import './AuthorPage.scss';
 
-const AuthorPage = (props) => {
+const AuthorPage = () => {
+  const [quoteList, setQuoteList] = useState([]);
+
+  const history = useHistory();
+
   const { author } = useParams();
   console.log(author);
 
-  return <div>{author}</div>;
-};
+  useEffect(() => {
+    const fetchQuoteByAuthor = async () => {
+      try {
+        const response = await quoteApi.getByAuthor(author);
+        setQuoteList(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-AuthorPage.propTypes = {};
+    fetchQuoteByAuthor();
+  }, [author]);
+
+  const handleBackClick = () => {
+    history.push('/');
+  };
+
+  return (
+    <div className='author-page'>
+      <h1 className='quote-author'>{author}</h1>
+
+      <ArrowBackIosIcon
+        fontSize='large'
+        className='quote-back'
+        onClick={handleBackClick}
+      />
+
+      <QuoteList quoteList={quoteList} />
+    </div>
+  );
+};
 
 export default AuthorPage;
